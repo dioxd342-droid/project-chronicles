eaa2894a3936d69d288464657cb1450dd2fe2823
dioxd342-droid <diox.d342@gmail.com>
2026-08-09 00:57:18 +0700

    feat: apply player class stats on join

--- src/main/java/com/projectchronicles/core/listener/PlayerJoinListener.java
+++ src/main/java/com/projectchronicles/core/listener/PlayerJoinListener.java
@@ -9,22 +9,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public final class PlayerJoinListener implements Listener {
-
     private final ChroniclesPlugin plugin;
-
     public PlayerJoinListener(ChroniclesPlugin plugin) { this.plugin = plugin; }
-
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         PlayerProfile profile = plugin.getPlayerManager().getProfile(event.getPlayer().getUniqueId());
+        plugin.getClassService().applyStats(event.getPlayer());
         String welcome = plugin.getConfig().getString("messages.welcome", "&6Добро пожаловать в &eProject Chronicles&6!");
-
         event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', welcome));
-        event.getPlayer().sendMessage(ChatColor.GRAY + "Твой уровень: " + ChatColor.YELLOW + profile.getLevel());
-
+        event.getPlayer().sendMessage(ChatColor.GRAY + "Уровень: " + ChatColor.YELLOW + profile.getLevel() + ChatColor.GRAY + " • Класс: " + ChatColor.YELLOW + profile.getPlayerClass());
         Quest firstSteps = plugin.getQuestService().getQuest("first_steps");
-        if (firstSteps != null && plugin.getQuestService().canComplete(firstSteps, event.getPlayer())) {
-            plugin.getQuestService().complete(firstSteps, event.getPlayer());
-        }
+        if (firstSteps != null && plugin.getQuestService().canComplete(firstSteps, event.getPlayer())) plugin.getQuestService().complete(firstSteps, event.getPlayer());
     }
 }
