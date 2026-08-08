17e406dfb31c34510524f693a885afc7013a4a58
dioxd342-droid <diox.d342@gmail.com>
2026-08-08 23:19:32 +0700

    feat: add player join handling

---
+++ src/main/java/com/projectchronicles/core/listener/PlayerJoinListener.java
@@ -0,0 +1,29 @@
+package com.projectchronicles.core.listener;
+
+import com.projectchronicles.core.ChroniclesPlugin;
+import com.projectchronicles.core.player.PlayerProfile;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public final class PlayerJoinListener implements Listener {
+
+    private final ChroniclesPlugin plugin;
+
+    public PlayerJoinListener(ChroniclesPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        PlayerProfile profile = plugin.getPlayerManager().getProfile(event.getPlayer().getUniqueId());
+        String welcome = plugin.getConfig().getString(
+                "messages.welcome",
+                "&6Добро пожаловать в &eProject Chronicles&6!"
+        );
+
+        event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', welcome));
+        event.getPlayer().sendMessage(ChatColor.GRAY + "Твой уровень: " + ChatColor.YELLOW + profile.getLevel());
+    }
+}
