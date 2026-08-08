e2462ad4fae38069df358e6c1456f03918cd1479
dioxd342-droid <diox.d342@gmail.com>
2026-08-09 00:29:24 +0700

    feat: integrate objective quest progression into server lifecycle

--- src/main/java/com/projectchronicles/core/ChroniclesPlugin.java
+++ src/main/java/com/projectchronicles/core/ChroniclesPlugin.java
@@ -15,33 +15,61 @@ import com.projectchronicles.core.npc.NpcInteractionListener;
 import com.projectchronicles.core.npc.NpcService;
 import com.projectchronicles.core.player.ExperienceService;
 import com.projectchronicles.core.player.PlayerManager;
+import com.projectchronicles.core.quest.ProgressQuestListener;
+import com.projectchronicles.core.quest.ProgressQuestService;
 import com.projectchronicles.core.quest.QuestService;
 import com.projectchronicles.core.world.WorldEventService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChroniclesPlugin extends JavaPlugin {
     private PlayerManager playerManager; private ExperienceService experienceService; private EconomyService economyService;
-    private QuestService questService; private FactionService factionService; private NpcService npcService;
+    private QuestService questService; private ProgressQuestService progressQuestService; private FactionService factionService; private NpcService npcService;
     private WorldEventService worldEventService; private DecisionService decisionService; private DonationService donationService;
     private CosmeticService cosmeticService; private CosmeticPetService cosmeticPetService;
+
     @Override public void onEnable() {
-        saveDefaultConfig(); playerManager = new PlayerManager(this); playerManager.initialize(); experienceService = new ExperienceService(this);
-        economyService = new EconomyService(this); questService = new QuestService(this); factionService = new FactionService(this); npcService = new NpcService(this);
+        saveDefaultConfig();
+        playerManager = new PlayerManager(this); playerManager.initialize();
+        experienceService = new ExperienceService(this); economyService = new EconomyService(this);
+        questService = new QuestService(this); progressQuestService = new ProgressQuestService(this);
+        factionService = new FactionService(this); npcService = new NpcService(this);
         worldEventService = new WorldEventService(this); decisionService = new DecisionService(this); donationService = new DonationService(this);
         cosmeticService = new CosmeticService(this); cosmeticPetService = new CosmeticPetService(this);
+
         ChroniclesCommand command = new ChroniclesCommand(this);
-        if (getCommand("chronicles") != null) { getCommand("chronicles").setExecutor(command); getCommand("chronicles").setTabCompleter(command); } else getLogger().severe("Command 'chronicles' is missing from plugin.yml");
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this); getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerActivityListener(this), this); getServer().getPluginManager().registerEvents(new NpcInteractionListener(this, npcService), this);
+        if (getCommand("chronicles") != null) {
+            getCommand("chronicles").setExecutor(command);
+            getCommand("chronicles").setTabCompleter(command);
+        } else getLogger().severe("Command 'chronicles' is missing from plugin.yml");
+
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerActivityListener(this), this);
+        getServer().getPluginManager().registerEvents(new NpcInteractionListener(this, npcService), this);
         getServer().getPluginManager().registerEvents(new CosmeticListener(this, cosmeticService), this);
+        getServer().getPluginManager().registerEvents(new ProgressQuestListener(this, progressQuestService), this);
+
         npcService.spawnStorytellerIfNeeded(); worldEventService.start();
         getServer().getScheduler().runTaskTimer(this, cosmeticService::tickEffects, 20L, 10L);
         getServer().getScheduler().runTaskTimer(this, cosmeticPetService::tick, 20L, 10L);
         getLogger().info("Project Chronicles loaded.");
     }
-    @Override public void onDisable() { if (worldEventService != null) worldEventService.stop(); if (cosmeticPetService != null) cosmeticPetService.shutdown(); if (playerManager != null) playerManager.shutdown(); }
-    public PlayerManager getPlayerManager() { return playerManager; } public ExperienceService getExperienceService() { return experienceService; }
-    public EconomyService getEconomyService() { return economyService; } public QuestService getQuestService() { return questService; } public FactionService getFactionService() { return factionService; }
-    public WorldEventService getWorldEventService() { return worldEventService; } public DecisionService getDecisionService() { return decisionService; } public DonationService getDonationService() { return donationService; }
-    public CosmeticService getCosmeticService() { return cosmeticService; } public CosmeticPetService getCosmeticPetService() { return cosmeticPetService; }
+
+    @Override public void onDisable() {
+        if (worldEventService != null) worldEventService.stop();
+        if (cosmeticPetService != null) cosmeticPetService.shutdown();
+        if (playerManager != null) playerManager.shutdown();
+    }
+
+    public PlayerManager getPlayerManager() { return playerManager; }
+    public ExperienceService getExperienceService() { return experienceService; }
+    public EconomyService getEconomyService() { return economyService; }
+    public QuestService getQuestService() { return questService; }
+    public ProgressQuestService getProgressQuestService() { return progressQuestService; }
+    public FactionService getFactionService() { return factionService; }
+    public WorldEventService getWorldEventService() { return worldEventService; }
+    public DecisionService getDecisionService() { return decisionService; }
+    public DonationService getDonationService() { return donationService; }
+    public CosmeticService getCosmeticService() { return cosmeticService; }
+    public CosmeticPetService getCosmeticPetService() { return cosmeticPetService; }
 }
