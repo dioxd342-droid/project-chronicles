78dfbf5d8d507bac3cfdcd24002e1c9382512046
dioxd342-droid <diox.d342@gmail.com>
2026-08-08 23:20:24 +0700

    feat: save player profile on quit

---
+++ src/main/java/com/projectchronicles/core/listener/PlayerQuitListener.java
@@ -0,0 +1,20 @@
+package com.projectchronicles.core.listener;
+
+import com.projectchronicles.core.ChroniclesPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public final class PlayerQuitListener implements Listener {
+
+    private final ChroniclesPlugin plugin;
+
+    public PlayerQuitListener(ChroniclesPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        plugin.getPlayerManager().remove(event.getPlayer().getUniqueId());
+    }
+}
