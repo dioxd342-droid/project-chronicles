3e6279076997be7796f1f3889319763294384bb3
dioxd342-droid <diox.d342@gmail.com>
2026-08-09 00:20:48 +0700

    fix: save cosmetic state on plugin shutdown

--- src/main/java/com/projectchronicles/core/ChroniclesPlugin.java
+++ src/main/java/com/projectchronicles/core/ChroniclesPlugin.java
@@ -18,57 +18,30 @@ import com.projectchronicles.core.world.WorldEventService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChroniclesPlugin extends JavaPlugin {
-    private PlayerManager playerManager;
-    private ExperienceService experienceService;
-    private EconomyService economyService;
-    private QuestService questService;
-    private FactionService factionService;
-    private NpcService npcService;
-    private WorldEventService worldEventService;
-    private DecisionService decisionService;
-    private DonationService donationService;
+    private PlayerManager playerManager; private ExperienceService experienceService; private EconomyService economyService;
+    private QuestService questService; private FactionService factionService; private NpcService npcService;
+    private WorldEventService worldEventService; private DecisionService decisionService; private DonationService donationService;
     private CosmeticService cosmeticService;
 
     @Override public void onEnable() {
         saveDefaultConfig();
         playerManager = new PlayerManager(this); playerManager.initialize();
-        experienceService = new ExperienceService(this);
-        economyService = new EconomyService(this);
-        questService = new QuestService(this);
-        factionService = new FactionService(this);
-        npcService = new NpcService(this);
-        worldEventService = new WorldEventService(this);
-        decisionService = new DecisionService(this);
-        donationService = new DonationService(this);
-        cosmeticService = new CosmeticService(this);
-
+        experienceService = new ExperienceService(this); economyService = new EconomyService(this); questService = new QuestService(this);
+        factionService = new FactionService(this); npcService = new NpcService(this); worldEventService = new WorldEventService(this);
+        decisionService = new DecisionService(this); donationService = new DonationService(this); cosmeticService = new CosmeticService(this);
         ChroniclesCommand command = new ChroniclesCommand(this);
-        if (getCommand("chronicles") != null) {
-            getCommand("chronicles").setExecutor(command);
-            getCommand("chronicles").setTabCompleter(command);
-        } else getLogger().severe("Command 'chronicles' is missing from plugin.yml");
-
+        if (getCommand("chronicles") != null) { getCommand("chronicles").setExecutor(command); getCommand("chronicles").setTabCompleter(command); }
+        else getLogger().severe("Command 'chronicles' is missing from plugin.yml");
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerActivityListener(this), this);
         getServer().getPluginManager().registerEvents(new NpcInteractionListener(this, npcService), this);
-        npcService.spawnStorytellerIfNeeded();
-        worldEventService.start();
-        getLogger().info("Project Chronicles loaded.");
+        npcService.spawnStorytellerIfNeeded(); worldEventService.start(); getLogger().info("Project Chronicles loaded.");
     }
-
-    @Override public void onDisable() {
-        if (worldEventService != null) worldEventService.stop();
-        if (playerManager != null) playerManager.shutdown();
-        getLogger().info("Project Chronicles disabled.");
-    }
-    public PlayerManager getPlayerManager() { return playerManager; }
-    public ExperienceService getExperienceService() { return experienceService; }
-    public EconomyService getEconomyService() { return economyService; }
-    public QuestService getQuestService() { return questService; }
-    public FactionService getFactionService() { return factionService; }
-    public WorldEventService getWorldEventService() { return worldEventService; }
-    public DecisionService getDecisionService() { return decisionService; }
-    public DonationService getDonationService() { return donationService; }
+    @Override public void onDisable() { if (worldEventService != null) worldEventService.stop(); if (playerManager != null) playerManager.shutdown(); if (cosmeticService != null) getServer().getOnlinePlayers().forEach(cosmeticService::save); }
+    public PlayerManager getPlayerManager() { return playerManager; } public ExperienceService getExperienceService() { return experienceService; }
+    public EconomyService getEconomyService() { return economyService; } public QuestService getQuestService() { return questService; }
+    public FactionService getFactionService() { return factionService; } public WorldEventService getWorldEventService() { return worldEventService; }
+    public DecisionService getDecisionService() { return decisionService; } public DonationService getDonationService() { return donationService; }
     public CosmeticService getCosmeticService() { return cosmeticService; }
 }
