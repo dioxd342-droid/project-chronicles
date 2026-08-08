ed86375ab66e051803bca8e3f543cc0c597fc1a8
dioxd342-droid <diox.d342@gmail.com>
2026-08-08 23:20:18 +0700

    feat: connect player manager to persistent storage

--- src/main/java/com/projectchronicles/core/player/PlayerManager.java
+++ src/main/java/com/projectchronicles/core/player/PlayerManager.java
@@ -10,27 +10,40 @@ public final class PlayerManager {
 
     private final ChroniclesPlugin plugin;
     private final Map<UUID, PlayerProfile> profiles = new ConcurrentHashMap<>();
+    private final PlayerDataStore dataStore;
 
     public PlayerManager(ChroniclesPlugin plugin) {
         this.plugin = plugin;
+        this.dataStore = new PlayerDataStore(plugin);
     }
 
     public void initialize() {
+        dataStore.initialize();
         plugin.getLogger().info("PlayerManager initialized.");
     }
 
     public PlayerProfile getProfile(UUID uniqueId) {
-        int startingLevel = plugin.getConfig().getInt("profile.starting-level", 1);
-        long startingExperience = plugin.getConfig().getLong("profile.starting-experience", 0);
-        return profiles.computeIfAbsent(uniqueId,
-                id -> new PlayerProfile(id, startingLevel, startingExperience));
+        return profiles.computeIfAbsent(uniqueId, dataStore::load);
+    }
+
+    public void saveProfile(UUID uniqueId) {
+        PlayerProfile profile = profiles.get(uniqueId);
+        if (profile != null) {
+            dataStore.save(profile);
+        }
+    }
+
+    public void saveAll() {
+        profiles.values().forEach(dataStore::save);
     }
 
     public void remove(UUID uniqueId) {
+        saveProfile(uniqueId);
         profiles.remove(uniqueId);
     }
 
     public void shutdown() {
+        saveAll();
         profiles.clear();
     }
 }
